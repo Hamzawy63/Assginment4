f276e94f7e677dabba904b5527a09e0692809ac6
Hamzawy63 <hamzahassan835@gmail.com>
2019-04-18 10:57:21 +0200

    Initial commit

---
+++ src/eg/edu/alexu/csd/datastructure/linkedList/cs26/cs59/myMain.java
@@ -0,0 +1,21 @@
+package eg.edu.alexu.csd.datastructure.linkedList.cs26.cs59;
+
+public class myMain {
+
+    public static void main(String[] args) {
+        singlyLinkedList list = new singlyLinkedList();
+        list.add(120);
+        list.add(1);
+        list.add(11);
+        list.add(1);
+        list.add(100);
+
+        list.print();
+
+
+        System.out.println(list.size());
+        System.out.println(list.contains(120));
+        System.out.println(list.get(2));
+    }
+
+}
