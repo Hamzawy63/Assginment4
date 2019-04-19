609c33c2c82d11e5f901460afe95e9f5db5ea620
Hamzawy63 <hamzahassan835@gmail.com>
2019-04-19 06:12:32 +0200

    some methods of polynomial interface are implemented

--- src/eg/edu/alexu/csd/datastructure/linkedList/cs26/cs59/Polynomial.java
+++ src/eg/edu/alexu/csd/datastructure/linkedList/cs26/cs59/Polynomial.java
@@ -7,7 +7,7 @@ public class Polynomial {
     singlyLinkedList B = new singlyLinkedList();
     singlyLinkedList C = new singlyLinkedList();
 
-    public void  setPolynomial(char poly, int[][] terms)
+    public void  setPolynomial(char poly, int[][] terms) /// not tested yet
     {
         singlyLinkedList tmp = new singlyLinkedList();
         Point term = new Point();
@@ -27,6 +27,55 @@ public class Polynomial {
 
         }
 
+    }
+    /**
+    public String print(char poly)
+    {
+        String representation = "";
+        representation.;
+    }
+    public String print(singlyLinkedList poly)
+    {
+        String representation = "";
+
+
+        singlyLinkedList.singlyLinkedListNode tmp = poly.head;
+        while(tmp!=null)
+        {
+            Point termPoint  =(Point) (tmp.value);
+            representation.;
+        }
+
+
+
+        return representation;
+    }
+    **/
+    public float evaluatePolynomial(char poly, float value) /// not tested yet
+    {
+        if(poly == 'A')
+            return evaluatePolynomial(A,value);
+        else if (poly == 'B')
+            return evaluatePolynomial(A,value);
+
+
+        return evaluatePolynomial(A,value);
+
+
+    }
+    public float evaluatePolynomial(singlyLinkedList poly, float value) /// method overloading
+    {
+        float result=0;
+        singlyLinkedList.singlyLinkedListNode tmp = poly.head; // a pointer at head node
+        while(tmp!=null)
+        {
+            Point term = (Point) tmp.value;
+            int x = term.x; /// coeffecient
+            int y = term.y; /// exponential
+            result += value*y+x;
+        }
+
+        return result;
     }
 
 }
